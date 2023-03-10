4776db6ba820ace939bf5b77da4742fb0e514302
Sayan Maity <89021746+Sayan-Maity@users.noreply.github.com>
2023-03-10 10:30:27 +0530

    code refactored

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express")
+const app = express();
+const PORT = 3001
+
+app.set ("view engine", "ejs")
+
+// middleware
+app.use(express.static("public"))
+
+
+app.get("/", (req, res) => {
+    res.status(200).render("index", {pageTitle : "Blog Page"})
+})
+app.get("/contact", (req, res) => {
+    res.status(200).render("contact", {pageTitle : "Contact"})
+})
+
+
+app.listen(3001, () => {
+    console.log(`server is running at port ${PORT}`)
+})
